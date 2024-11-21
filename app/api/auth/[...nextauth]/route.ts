94165fda38f8c77e3926d361a62523250022a032
Diwan Malla <diwanmalla@192-168-1-5.tpgi.com.au>
2024-11-21 22:33:38 +1100

    .

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,10 @@
 // app/api/auth/[...nextauth]/route.ts
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/authOptions";
+import { NextApiRequest, NextApiResponse } from "next";
 
-// Export the handler as default
-export default NextAuth(authOptions);
+// Export the handler for both GET and POST methods
+export const GET = (req: NextApiRequest, res: NextApiResponse) =>
+  NextAuth(req, res, authOptions);
+export const POST = (req: NextApiRequest, res: NextApiResponse) =>
+  NextAuth(req, res, authOptions);
