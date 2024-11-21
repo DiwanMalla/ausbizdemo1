30dfc4017450464a4c6e2e19fbae1e324935b44c
Diwan Malla <diwanmalla@192-168-1-5.tpgi.com.au>
2024-11-21 22:29:21 +1100

    Fix typescript

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,56 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { authOptions } from "@/lib/authOptions";
 import NextAuth from "next-auth";
-import WordPressProvider from "next-auth/providers/wordpress";
-import { JWT } from "next-auth/jwt";
-import { Session } from "next-auth";
-
-export const authOptions = {
-  providers: [
-    WordPressProvider({
-      clientId: process.env.WORDPRESS_CLIENT_ID,
-      clientSecret: process.env.WORDPRESS_CLIENT_SECRET,
-      token: {
-        url: "https://public-api.wordpress.com/oauth2/token",
-        async request(context) {
-          const { provider, params: parameters, checks, client } = context;
-
-          const tokenset = await client.grant({
-            grant_type: "authorization_code",
-            code: parameters.code,
-            redirect_uri: provider.callbackUrl as string, // Ensure this matches your actual redirect URI
-            code_verifier: checks.code_verifier,
-            client_id: process.env.WORDPRESS_CLIENT_ID,
-            client_secret: process.env.WORDPRESS_CLIENT_SECRET,
-          });
-
-          return { tokens: tokenset };
-        },
-      },
-    }),
-  ],
-  pages: {
-    signIn: "/auth/signin", // Define this route in your Next.js project
-    error: "/auth/error", // Define this route in your Next.js project
-  },
-  callbacks: {
-    async redirect({ url, baseUrl }: { url: string; baseUrl: string }) {
-      return url.startsWith(baseUrl) ? url : baseUrl;
-    },
-    async jwt({ token, account }: { token: JWT; account: any }) {
-      if (account?.provider === "wordpress") {
-        token.accessToken = account.access_token; // Include access token if needed
-      }
-      return token;
-    },
-    async session({ session, token }: { session: Session; token: JWT }) {
-      if (token?.accessToken) {
-        (session as { accessToken?: string }).accessToken =
-          token.accessToken as string;
-      }
-      return session;
-    },
-  },
-};
 
 // Ensure GET and POST methods are exported
 export const handler = NextAuth(authOptions);
--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,6 +1,6 @@
 import Navbar from "@/components/header";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/authOptions";
 import { Link } from "lucide-react";
 
 // app/blog/layout.tsx
