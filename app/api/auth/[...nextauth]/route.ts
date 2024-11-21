a1bd46fca0542ef364c0743d9d1daaae8b57bee4
Diwan Malla <diwanmalla@192-168-1-5.tpgi.com.au>
2024-11-22 01:11:49 +1100

    back to previous

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,6 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextRequest, NextResponse } from "next/server";
+import { authOptions } from "@/lib/authOptions";
 import NextAuth from "next-auth";
-import { authOptions } from "@/lib/authOptions"; // Assuming you exported your authOptions from a separate file
 
-// Export GET and POST methods
-export async function GET(req: NextRequest) {
-  const res = NextResponse.next(); // Create a response object
-  return handleAuth(req, res);
-}
-
-export async function POST(req: NextRequest) {
-  const res = NextResponse.next(); // Create a response object
-  return handleAuth(req, res);
-}
-
-// Custom handler to invoke NextAuth with the correct request and response
-async function handleAuth(req: NextRequest, res: NextResponse) {
-  // Type-casting req to any and passing res to match NextAuth's expected types
-  return NextAuth(req as any, res as any, authOptions);
-}
+// Ensure GET and POST methods are exported
+export const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
